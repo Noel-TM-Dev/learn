2415d4d2dc114718cf818e05b91bf4d4d4a7b983
Noel Trejo <noeltrejom16@gmail.com>
2022-12-19 12:54:10 -0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn';
+
+   nombre :string = "Noel Trejo"
+
+
+   mostrarNombre(){
+    this.nombre = "NOEL";
+   }
+
 }
