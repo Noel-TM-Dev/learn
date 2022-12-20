bf19330dfcea53f81de14a7a091d3fa85ab8e867
Noel Trejo <noeltrejom16@gmail.com>
2022-12-19 17:55:40 -0700

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
+import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
+import { OrdenarComponent } from './ventas/pages/ordenar/ordenar.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BasicosComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'numeros',
+    component: NumerosComponent
+  },
+  {
+    path: 'no-comunes',
+    component: NoComunesComponent
+  },
+  {
+    path: 'ordenar',
+    component: OrdenarComponent
+  },
+  //se agregar el comodin
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
 
@@ -15,7 +35,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
